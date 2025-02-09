d7a231f3728234713e520ba307b00b8644ee2c40
atomarthur <arthur22rai@gmail.com>
2025-02-09 18:41:07 -0300

    No processo de arrumar a tabela de jogos

---
+++ php/competicoes.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/sportif.css">
+    <title>SportIF</title>
+</head>
+<body>
+    <header>
+        <h1>SportIF</h1>
+        <div class="logo_container">
+        <img src="../imagens/logo.jpg" alt="Logo do IFBA" class="logo">
+    </div>
+    </header>
+    <nav>
+        <a href="../php/cadastro.php">Inscrição</a>
+        <a href="notificacoes.html">Notificação</a>
+        <a href="classificacao.html">Classificação</a>
+        <a href="competicoes.html">Competições</a>
+        <a href="estatiscticas.html">Estatísticas</a>
+        <a href="placar.html">Placar</a>
+        <a href="suporte.html">Suporte</a>
+    </nav>
+
+    <div class="tabela">
+        
+    </div>
+
+    <br><br><br><br> <br><br><br><br> <br><br><br><br><br><br><br><br><br><br><br><br><br><br> <br><br><br><br> <br><br><br><br> <br><br><br><br><br><br><br><br>
+
+    <footer>
+        <p>&copy;2024 SportIF. Todos os direitos reservados.</p>
+    </footer>
+    
+</body>
