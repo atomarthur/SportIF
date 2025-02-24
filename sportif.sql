7ff640b075bed4e80508f69982a62e7f46bc5e85
atomarthur <arthur22rai@gmail.com>
2025-02-24 14:08:28 -0300

    SQL modificado

--- sportif.sql
+++ sportif.sql
@@ -42,28 +42,23 @@ CREATE TABLE jogos (
     modalidade_id INT NOT NULL,
     placar_equipe_a INT DEFAULT 0,
     placar_equipe_b INT DEFAULT 0,
-    status_jogo ENUM('agendado', 'em andamento', 'finalizado'),
+    status_jogo ENUM('Agendado', 'Em Andamento', 'Finalizado'),
     FOREIGN KEY (equipe_a_id) REFERENCES equipes(id_time) ON DELETE CASCADE,
     FOREIGN KEY (equipe_b_id) REFERENCES equipes(id_time) ON DELETE CASCADE,
     FOREIGN KEY (modalidade_id) REFERENCES modalidades(id_modalidade) ON DELETE CASCADE,
     CONSTRAINT check_equipes_diferentes CHECK (equipe_a_id <> equipe_b_id)
 );
 
-INSERT INTO jogos(equipe_a_id, equipe_b_id, modalidade_id, placar_equipe_a, placar_equipe_b, status) 
-VALUES (1, 2, 1, 1, 7, "finalizado") ;
+CREATE TABLE notificacoes (
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+titulo VARCHAR (100),
+mensagem VARCHAR(255)
+);
 
-SELECT 
-    jogos.id,
-    equipe_a.nome AS equipe_a,
-    equipe_b.nome AS equipe_b,
-    modalidades.nome AS modalidade,
-    jogos.placar_equipe_a,
-    jogos.placar_equipe_b,
-    jogos.status_jogo
-FROM jogos
-JOIN equipes AS equipe_a ON jogos.equipe_a_id = equipe_a.id_time
-JOIN equipes AS equipe_b ON jogos.equipe_b_id = equipe_b.id_time
-JOIN modalidades ON jogos.modalidade_id = modalidades.id_modalidade;
-
-
-
+CREATE TABLE estatisticas (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id_jogo INT NOT NULL,
+    estatistica_nome VARCHAR(100) NOT NULL,
+    valor INT NOT NULL,
+    FOREIGN KEY (id_jogo) REFERENCES jogos(id) ON DELETE CASCADE
+);
